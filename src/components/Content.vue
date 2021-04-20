0eaf89383030639207f18d4c00f967c8c582560d
Riley Foster <riley.foster1989@gmail.com>
2021-04-20 02:59:11 -0500

    Clickable backgrounnd.

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,24 +1,68 @@
 <template>
   <div
     class="aniron content-container content-image opacity-50 dark:content-image-dark text-onBackground dark:text-onBackground-dark"
-  ></div>
+    :class="theme"
+    v-on:click="click"
+  >
+    {{ message }}
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      darkMode: false,
+    };
+  },
+  methods: {
+    click: function () {
+      let el = document.getElementsByTagName("html").item(0);
+
+      if (el.className === "dark") {
+        el.className = "light";
+      } else {
+        el.className = "dark";
+      }
+
+      this.darkMode = el.className === "dark";
+    },
+  },
+  computed: {
+    theme: function () {
+      return this.darkMode ? "content-image-dark" : "content-image-light";
+    },
+    position: function () {
+      return "";
+    },
+    message: function () {
+      return this.darkMode
+        ? "You! Shall not, pass!!"
+        : "Speak friend and enter...";
+    },
+  },
+};
 </script>
 
 <style scoped>
 .content-container {
   position: relative;
   height: calc(100vh - var(--header-height));
-  text-align: center;
-  padding-top: 5px;
   font-size: 20px;
-}
-.content-image {
-  background-image: url("../assets/Gandalf3.jpg");
   background-repeat: no-repeat;
   background-size: cover;
+}
+.content-image-dark {
+  background-image: url("../assets/Gandalf4.jpg");
+  opacity: 0.2;
+  text-align: left;
+  padding-top: 100px;
+  padding-left: 50px;
+}
+.content-image-light {
+  background-image: url("../assets/Gandalf3.jpg");
+  opacity: 0.5;
+  text-align: center;
+  padding-top: 50px;
 }
 </style>
