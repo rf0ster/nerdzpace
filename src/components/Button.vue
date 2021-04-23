d6c1ba72d6793918d869e39710d113aa1c7449e7
Riley Foster <riley.foster1989@gmail.com>
2021-04-23 03:07:24 -0500

    First Login Box.

---
+++ src/components/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button
+      @mousedown="mouseDown"
+      @mouseover="mouseOver"
+      @mouseleave="mouseLeave"
+      :id="id"
+    >
+      {{ name }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "id"],
+  methods: {
+    mouseOver: function () {
+      document.getElementById(this.id).focus();
+    },
+    mouseLeave: function () {
+      document.getElementById(this.id).blur();
+    },
+    mouseDown: function () {},
+  },
+};
+</script>
+
+<style scoped>
+button {
+    padding: 8px;
+}
+button:active {
+    opacity: 0.5;
+};
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,15 +3,38 @@
     class="flex flex-col opacity-60 justify-center aniron w-screen h-screen bg-moria"
   >
     <div
-      class="flex justify-center bg-surface text-onSurface mr-auto ml-auto pt-3 h-1/2 w-1/4 rounded-3xl"
+      class="flex flex-col bg-surface text-onSurface mr-auto ml-auto pt-3 h-1/2 w-1/4 rounded-3xl b-shad"
     >
-      Speak friend and enter...
+      <div class="ml-auto mr-auto">Speak friend and enter...</div>
+      <div class="ml-auto mr-auto pt-10 flex flex-col justify-center">
+        <TextInput
+          class="mt-15"
+          id="email"
+          name="Email"
+          placeholder="email address..."
+        />
+        <TextInput
+          class="mt-8"
+          id="password"
+          name="Password"
+          placeholder="password..."
+        />
+      </div>
+      <Button class="mt-auto ml-auto mr-auto mb-3" name="Login" id="login" />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import TextInput from "@/components/TextInput.vue";
+import Button from "@/components/Button.vue";
+
+export default {
+  components: {
+    TextInput,
+    Button,
+  },
+};
 </script>
 
 <style scoped>
@@ -19,5 +42,10 @@ export default {};
   background-image: url("../assets/Moria.jpg");
   background-repeat: no-repeat;
   background-size: cover;
+}
+.b-shad {
+    box-shadow: 30px 25px 30px;
+    border: 1px solid;
+    border-style: ridge;
 }
 </style>
